556b058dbc8cda4527c034e7310737a149adfc22
ekribil <kristijan.bilic@ericsson.com>
2018-03-28 08:19:52 +0200

    Added support for method OPTIONS and CORS

--- src/main/java/com/krisbijan/oauth2_krisbijan_server/config/WebSecurityConfiguration.java
+++ src/main/java/com/krisbijan/oauth2_krisbijan_server/config/WebSecurityConfiguration.java
@@ -1,14 +1,29 @@
 package com.krisbijan.oauth2_krisbijan_server.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.krisbijan.oauth2_krisbijan_server.service.user.CustomUserDetailsService;
 
@@ -36,5 +51,56 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
 		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// return new WebMvcConfigurerAdapter() {
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// registry.addMapping("/**").allowedMethods("GET", "POST", "PUT",
+	// "DELETE").allowedOrigins("*")
+	// .allowedHeaders("*");
+	// }
+	// };
+	// }
+	//
+	// @Bean
+	// CorsConfigurationSource corsConfigurationSource() {
+	// CorsConfiguration configuration = new CorsConfiguration();
+	// configuration.setAllowedOrigins(Arrays.asList("http://myufrontend.com"));
+	// configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+	// UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// source.registerCorsConfiguration("/**", configuration);
+	// return source;
+	// }
+	//
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// http.cors().and();
+	// }
+	//
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+
+	@Bean
+	CorsFilter corsFilter() {
+		CorsFilter filter = new CorsFilter();
+		return filter;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.addFilterBefore(corsFilter(), SessionManagementFilter.class).formLogin()
+				.loginProcessingUrl("/authentication").passwordParameter("password").usernameParameter("username").and()
+				.logout().deleteCookies("JSESSIONID").invalidateHttpSession(true).logoutUrl("/logout")
+				.logoutSuccessUrl("/").and().csrf().disable().anonymous().disable().authorizeRequests()
+				.antMatchers("/authentication").permitAll().antMatchers("/oauth/token").permitAll()
+				.antMatchers("/admin/*").access("hasRole('ROLE_ADMIN')").antMatchers("/user/*")
+				.access("hasRole('ROLE_USER')");
 	}
 }
